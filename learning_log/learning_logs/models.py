d5e7dce4aa5fb69547439dce8450b202318dcf08
Nikita Khomenko <mishanikita261202@mail.ru>
2024-11-08 15:12:22 +0300

    Create models.py

---
+++ learning_log/learning_logs/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Topic(models.Model):
+    """Тема, которую изучает пользователь."""
+    
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        """Возвращает строковое представление модели."""
+        
+        return self.text
+    
+class Entry(models.Model):
+    """Информация, изученная пользователем по теме."""
+    
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'entries'
+        
+    def __str__(self):
+        """Возвращает строковое представление модели."""
+        
+        return f"{self.text[:50]}..."
