38a3a909f0f044965996e794b31f1b679912a0b1
Nikita Khomenko <mishanikita261202@mail.ru>
2024-11-11 14:55:49 +0300

    Update views.py

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 def index(request):
     """Домашняя страница приложения Learning Log."""
@@ -23,3 +24,38 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Определяет новую тему."""
+    if request.method != 'POST':
+        # Данные не отправлялись; создается пустая форма.
+        form = TopicForm()
+    else:
+        # Отправлены данные POST; обработать данные.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+        
+    # Вывести пустую или недействительную форму.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Добавляет новую запись по конкретной теме."""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # Данные не отправлялись; создается пустая форма.
+        form = EntryForm()
+    else:
+        # Отправлены данные POST; обработать данные.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+        
+    # вывести пустую или недействительную форму.
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
