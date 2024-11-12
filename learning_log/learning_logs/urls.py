a9c91a77aacc39b28ef26e92c535fb6990ee8ce0
Nikita Khomenko <mishanikita261202@mail.ru>
2024-11-12 17:48:23 +0300

    Update urls.py

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -16,4 +16,6 @@ urlpatterns = [
     path('new_topic/', views.new_topic, name='new_topic'),
     # Страница для добавления новой записи
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Страница для редактирования записи
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
