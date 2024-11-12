86f56b7658d230c2169ca6abe734cead92f16f5f
Nikita Khomenko <mishanikita261202@mail.ru>
2024-11-12 17:48:24 +0300

    Update views.py

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 def index(request):
@@ -59,3 +59,21 @@ def new_entry(request, topic_id):
     # вывести пустую или недействительную форму.
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """Редактирует существующую запись."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    
+    if request.method != 'POST':
+        # Исходный запрос; форма заполняется данными текущей записи.
+        form = EntryForm(instance=entry)
+    else:
+        # Отправка данных POST; обработать данные.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+        
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
