709abf1056538a24e0058500bf845cca1dd58047
Nikita Khomenko <mishanikita261202@mail.ru>
2024-11-12 18:54:24 +0300

    Update urls.py

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -1,4 +1,4 @@
-"""Определяет схемы URL для learning_logs."""
+"""Определяет схемы URL для learning_logs"""
 
 from django.urls import path
 
@@ -12,10 +12,10 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     # Страница с подробной информацией по отдельной теме.
     path('topics/<int:topic_id>/', views.topic, name='topic'),
-    # Страница для добавления новой темы
+    # Страница для добавления новой темы.
     path('new_topic/', views.new_topic, name='new_topic'),
-    # Страница для добавления новой записи
+    # Страница для добавления новой записи.
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
-    # Страница для редактирования записи
+    # Страница для редактирования записи.
     path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
