44268f66a8f0635999cc00179cbf39d63238be19
Nikita Khomenko <mishanikita261202@mail.ru>
2024-11-13 13:00:21 +0300

    Update urls.py

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     # Включить URL авторизации по умолчанию.
     path('', include('django.contrib.auth.urls')),
+    # Страница регистрации.
+    path('register/', views.register, name='register'),
 ]
