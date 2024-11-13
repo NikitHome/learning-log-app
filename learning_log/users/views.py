6a139522c56d14c764397489a5343fa8d764b7aa
Nikita Khomenko <mishanikita261202@mail.ru>
2024-11-13 13:00:22 +0300

    Update views.py

--- learning_log/users/views.py
+++ learning_log/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    """Регистрирует нового пользователя."""
+    if request.method != 'POST':
+        # Выводит пустую форму регистрации.
+        form = UserCreationForm()
+    else:
+        # Обработка заполненной формы.
+        form = UserCreationForm(data=request.POST)
+        
+        if form.is_valid():
+            new_user = form.save()
+            # Выполнение входа и перенаправление на домашнюю страницу.
+            login(request, new_user)
+            return redirect('learning_logs:index')
+        
+    # Вывести пустую или недействительную форму.
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
